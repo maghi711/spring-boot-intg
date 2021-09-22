b70b4817646472103a05d532f7f8732b0b6c178a
Mahesh J <mahesh.jyothi.ramalingam@signify.com>
2021-09-22 12:28:00 +0530

    Choosing a password encoder using Delegating Password Encoder.

--- src/test/java/com/boot/app/SecurityTests.java
+++ src/test/java/com/boot/app/SecurityTests.java
@@ -2,9 +2,14 @@ package com.boot.app;
 
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SecurityTests {
 
@@ -12,9 +17,15 @@ public class SecurityTests {
     public void bcryptEncoderCheck() {
         System.out.println(new BCryptPasswordEncoder().encode("aadavan")); // medium strong password authentication
         System.out.println(new Pbkdf2PasswordEncoder().encode("aadavan")); // more stronger password authentication
-        //System.out.println(new NoOpPasswordEncoder[].encode("aadavan")); // plain text password, don't use in prod, use for testing purposes only.
-
         System.out.println(new SCryptPasswordEncoder().encode("aadavan")); // much more stronger password authentication
-        // java.lang.NoClassDefFoundError: org/bouncycastle/crypto/generators/SCrypt
+
+        // Define a map of password encoders
+        Map<String, PasswordEncoder> passwordEncoders = new HashMap<>();
+        passwordEncoders.put("bcrypt", new BCryptPasswordEncoder());
+        passwordEncoders.put("scrypt", new SCryptPasswordEncoder());
+
+        // choose them at run time based on the value available in the header or the body.
+        final DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder("scrypt", passwordEncoders);
+        System.out.println(delegatingPasswordEncoder.encode("aadavan"));
     }
 }
