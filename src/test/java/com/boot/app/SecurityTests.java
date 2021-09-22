664c198181435083dcb4a55f05a540b5d29e0de6
Mahesh J <mahesh.jyothi.ramalingam@signify.com>
2021-09-22 07:45:04 +0530

    Added many other password encoders in the application.

--- src/test/java/com/boot/app/SecurityTests.java
+++ src/test/java/com/boot/app/SecurityTests.java
@@ -2,11 +2,19 @@ package com.boot.app;
 
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 public class SecurityTests {
 
     @Test
     public void bcryptEncoderCheck() {
-        System.out.println(new BCryptPasswordEncoder().encode("aadavan"));
+        System.out.println(new BCryptPasswordEncoder().encode("aadavan")); // medium strong password authentication
+        System.out.println(new Pbkdf2PasswordEncoder().encode("aadavan")); // more stronger password authentication
+        //System.out.println(new NoOpPasswordEncoder[].encode("aadavan")); // plain text password, don't use in prod, use for testing purposes only.
+
+        //System.out.println(new SCryptPasswordEncoder().encode("aadavan")); // much more stronger password authentication
+        // java.lang.NoClassDefFoundError: org/bouncycastle/crypto/generators/SCrypt
     }
 }
